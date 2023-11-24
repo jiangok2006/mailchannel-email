bfbef6eaefb9ce982a9b0593f2185111568e410c
Lian Jiang <lian.jiang@opendoor.com>
2023-11-23 21:44:58 -0800

    try mailchannel

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/cloudflare";
+import { Form } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,33 +10,17 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <Form data-static-form-name="contact">
+      <p>
+        <label>Email address <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <label>Message <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">send</button>
+      </p>
+    </Form>
+
   );
 }
---
+++ functions/_middleware.ts
@@ -0,0 +1,18 @@
+import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
+
+export const onRequest: PagesFunction = mailChannelsPlugin({
+    personalizations: [
+        {
+            to: [{ name: "ACME Support", email: "support@example.com" }],
+        },
+    ],
+    from: {
+        name: "ACME Support",
+        email: "support@example.com",
+    },
+    respondWith: () => {
+        return new Response(
+            `Thank you for submitting your enquiry. A member of the team will be in touch shortly.`
+        );
+    },
+});
